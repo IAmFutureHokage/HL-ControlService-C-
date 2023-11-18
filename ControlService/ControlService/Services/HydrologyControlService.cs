fb8b7a11988db231e53dc14f6c99c515ee6db7c0
DirtyCode47 <alexprog47@mail.ru>
2023-11-19 08:52:45 +1000

    mini fix

---
+++ ControlService/ControlService/Services/HydrologyControlService.cs
@@ -0,0 +1,14 @@
+﻿using ControlService.Protos;
+using Grpc.Core;
+using ControlService.Models.Repositories;
+
+namespace ControlService.Services
+{
+    public class HydrologyControlService
+    {
+        public Task<GetResponse> Get(GetRequest request, ServerCallContext context)
+        {
+            
+        }
+    }
+}
