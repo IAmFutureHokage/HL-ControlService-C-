bb219c631bdb39dd88a23c7117035d09e384f08f
DirtyCode47 <alexprog47@mail.ru>
2023-11-24 21:04:19 +1000

    Fixed gRPC problem with timestamp

--- ControlService/ControlService/Protos/HydrologyControlServiceImpl.cs
+++ ControlService/ControlService/Protos/HydrologyControlServiceImpl.cs
@@ -3,6 +3,8 @@ using ControlService.Models.Repositories;
 using Microsoft.VisualBasic;
 using ControlService.Models.Entities;
 using Microsoft.AspNetCore.Components;
+using Google.Protobuf.WellKnownTypes;
+using System;
 
 namespace ControlService.Protos
 {
@@ -29,7 +31,7 @@ namespace ControlService.Protos
         {
 
             GetResponse response = new GetResponse();
-            response.HydrologyControls.Add(new HydrologyControl { Id = "123", Dateend = "123", Datestart = "123", PostCode = 1, Type = 1, Value = 2 });
+            //response.HydrologyControls.Add(new HydrologyControl { Id = "123", Dateend = "123", Datestart = "123", PostCode = 1, Type = 1, Value = 2 });
             return Task.FromResult(response);
         }
         public override Task<UpdateResponse> Update(UpdateRequest request, ServerCallContext context)
@@ -48,14 +50,22 @@ namespace ControlService.Protos
         }
         public override Task<GetDateResponse> GetDate(GetDateRequest request, ServerCallContext context)
         {
-
+            //google.protobuf.Timestamp date = 1;
+            //uint32 norm = 2;
+            //uint32 floodplain = 3;
+            //uint32 adverse = 4;
+            //uint32 dangerous = 5;
+            //var allnfad = new AllNFAD() { Date = Timestamp.FromDateTime(new DateTime(2023, 1, 1, 12, 0, 0, DateTimeKind.Utc)), Dangerous = 123, Adverse = 123, Floodplain = 123, Norm = 123 };
+            //var nfad = new NFAD() { Id = "123", DateStart = Timestamp.FromDateTime(new DateTime(2023, 1, 1, 0, 0, 0, DateTimeKind.Utc)), NextId = "213", PrevId = "1312", PostCode = 1, Type = ControlType.Norm, Value = 213 };
+            //var nfad = new NFAD() { Id = "123", DateStart = Timestamp.FromDateTime(new DateTime(2023, 1, 1, 0, 0, 0, DateTimeKind.Utc)), NextId = "213", PrevId = "1312", PostCode = 1, Type = ControlType.Norm, Value = 213 };
+            //var response = new GetDateResponse(new GetDateResponse() { Data = allnfad });
             var response = new GetDateResponse();
             //response.HydrologyControls.Add(new HydrologyControl { Id = "123", Dateend = "123", Datestart = "123", PostCode = 1, Type = 1, Value = 2 });
             return Task.FromResult(response);
         }
-        public override Task<GateIntervalResponse> GateInterval(GateIntervalRequest request, ServerCallContext context)
+        public override Task<GetIntervalResponse> GetInterval(GetIntervalRequest request, ServerCallContext context)
         {
-            var response = new GateIntervalResponse();
+            var response = new GetIntervalResponse();
             //response.HydrologyControls.Add(new HydrologyControl { Id = "123", Dateend = "123", Datestart = "123", PostCode = 1, Type = 1, Value = 2 });
             return Task.FromResult(response);
         }
